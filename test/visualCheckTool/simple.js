5a738f0bbacf370e58a1d8e278df249e27d69848
OHTSUKA Ko-hei <kochizufan@gmail.com>
2013-01-10 10:12:42 +0900

    visualCheckToolを追加、テストケース追加
    Signed-off-by: OHTSUKA Ko-hei <kochizufan@gmail.com>

---
+++ test/visualCheckTool/simple.js
@@ -0,0 +1,87 @@
+/// COPYRIGHT 2013 GEOHEX Inc. ///
+
+var stylez = [
+  {
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "invert_lightness": true },
+      { "color": "#4a4a4a" },
+      { "lightness": -10 }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "color": "#000300" }
+    ]
+  },{
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "administrative",
+    "elementType": "labels.icon",
+    "stylers": [
+      { "invert_lightness": true },
+      { "weight": 1 },
+      { "visibility": "off" }
+    ]
+  },{
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "saturation": -100 },
+      { "invert_lightness": true }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "color": "#575a5a" }
+    ]
+  },{
+    "featureType": "transit",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#808080" }
+    ]
+  },{
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#7e8080" },
+      { "lightness": -42 }
+    ]
+  },{
+    "featureType": "poi.business",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#707070" },
+      { "lightness": 25 }
+    ]
+  },{
+    "featureType": "administrative.province",
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "on" },
+      { "invert_lightness": true },
+      { "lightness": 60 }
+    ]
+  },{
+    "featureType": "administrative.country",
+    "elementType": "labels.text",
+    "stylers": [
+      { "visibility": "on" },
+      { "invert_lightness": true },
+      { "lightness": 49 }
+    ]
+  },{
+    "featureType": "transit.station",
+    "elementType": "labels.text",
+    "stylers": [
+      { "invert_lightness": true },
+      { "visibility": "on" },
+      { "saturation": -100 }
+    ]
+  }
+];
